fcac0ebaa517da3e3eb3746475fc78a9ae7b7719
Rikentod <annadiumheart@gmail.com>
2026-05-09 17:19:54 +0200

    added bare minimum to get standard actions working

---
+++ js/data_standard.js
@@ -0,0 +1,24 @@
+data_standard = [
+    {
+        title: "Aura Maneuver [1/Round]",
+        icon: "juggler",
+        subtitle: "Activate an Aura",
+        description: "Activate an Aura you have access to.",
+        bullets: [
+            "Action Cost: 1 Action",
+            "KP Cost: Varies (each Aura will have their own KP Cost, that is the KP Cost you pay for this Maneuver)",
+            "Effect: Activate an Aura you have access to. At the start of each of your turns when an Aura is active, you must pay 1/2 of the KP Cost or deactivate that Aura."
+        ]
+    },
+    {
+        title: "Basic Attack Maneuver",
+        icon: "crossed-swords",
+        subtitle: "Make a basic, unnamed attack.",
+        description: "Make a basic, unnamed attack.",
+        bullets: [
+            "Action Cost: 1 Action",
+            "KP Cost: Varies (uses the Ki Point Cost of the chosen Profile)",
+            "Effect: Target an Opponent with an Attacking Maneuver using a Profile (see : Attacking) of your choice. If the Profile has an Area of Effect, use that to select the target(s) instead. You can only use each Profile once per Combat Round when using a Basic Attack Maneuver, except for the Simple, Sphere and Spell Profiles."
+        ]
+    }
+]
--- js/quickref.js
+++ js/quickref.js
@@ -57,6 +57,7 @@ function fill_section(data, parentname, type) {
 }
 
 function init() {
+    fill_section(data_standard, "standard_actions", "Standard Action")
     fill_section(data_movement, "basic-movement", "Move");
     fill_section(data_action, "basic-actions", "Action");
     fill_section(data_bonusaction, "basic-bonus-actions", "Bonus action");
